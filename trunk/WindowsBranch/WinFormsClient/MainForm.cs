36ea35bd073ed23e475b6e57d5b96d53054e0311
jopsen <jopsen@9db144e2-f328-0410-9072-b3fcc14a7c39>
2007-02-15 17:06:03 +0000

    First working windows and os x... only runs correctly if compiled with mono....

--- trunk/WindowsBranch/WinFormsClient/MainForm.cs
+++ trunk/WindowsBranch/WinFormsClient/MainForm.cs
@@ -29,8 +29,10 @@ namespace WinFormsClient
 		
 		protected LibLastRip.LastManager Manager;
 		protected Settings settings;
+		protected System.Int32 TrackDuration = 0;
 		protected const System.Int32 UpdateInterval = 6000;
-		public System.Windows.Forms.Timer Timer;
+		public System.Timers.Timer Timer = new System.Timers.Timer(MainForm.UpdateInterval);
+		
 		public MainForm()
 		{
 			//
@@ -49,20 +51,16 @@ namespace WinFormsClient
 				this.RadioStation.Enabled = true;
 				this.TuneInButton.Enabled = true;
 			}
-			this.Timer = new System.Windows.Forms.Timer();
-			this.Timer.Interval = MainForm.UpdateInterval;
-			
 			this.Manager.OnNewSong += new EventHandler(this.OnNewSong);
-			//this.Timer.Tick  += new System.Timers.ElapsedEventHandler(this.Manager.UpdateMetaInfo);
-			this.Timer.Tick  += new System.EventHandler(this.UpdateProgress);
-			this.Timer.Enabled = false;
+			this.Timer.Elapsed += new System.Timers.ElapsedEventHandler(this.Manager.UpdateMetaInfo);
+			this.Timer.Elapsed += new System.Timers.ElapsedEventHandler(this.UpdateProgress);
 		}
 		
 		protected virtual void OnNewSong(System.Object Sender, System.EventArgs Args)
 		{
-			System.Windows.Forms.MessageBox.Show("New song!");
 			LibLastRip.MetaInfo Info = (LibLastRip.MetaInfo)Args;
 			this.SetStatus(Info);
+			this.TrackDuration = 0;
 			this.StatusBar.Value = 0;
 			this.StatusBar.Maximum = System.Convert.ToInt32(Info.Trackduration);
 		}
@@ -74,10 +72,10 @@ namespace WinFormsClient
 				this.StatusLabel.Text = Info.ToString(); //TODO: better formatting
 				if(Info.AlbumcoverSmall != null && Info.AlbumcoverSmall.StartsWith("http://"))
 				{
-					//TODO: Port to mono OR patch mono
+					//TODO: Port to mono
 					try
 					{
-					//	this.StatuspictureBox.Load(Info.AlbumcoverSmall);
+						//this.StatuspictureBox.Load(Info.AlbumcoverSmall);
 					}
 					catch
 					{}
@@ -134,15 +132,13 @@ namespace WinFormsClient
 		
 		void TuneInButtonClick(object sender, EventArgs e)
 		{
-			this.TuneInButton.Enabled = false;
 			if(this.Manager.ChangeStation(this.RadioStation.Text))
 			{
 				this.LoveButton.Enabled = true;
 				this.HateButton.Enabled = true;
 				this.SkipButton.Enabled = true;
-				this.Timer.Enabled = true;
+				this.Timer.Start();
 			}
-			this.TuneInButton.Enabled = true;
 		}
 		
 		void SkipButtonClick(object sender, EventArgs e)
