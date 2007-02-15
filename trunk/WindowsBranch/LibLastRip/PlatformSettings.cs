e8785e0a70455acdac1c82e52cf1897dc64f10a3
jopsen <jopsen@9db144e2-f328-0410-9072-b3fcc14a7c39>
2007-02-15 13:59:53 +0000

    First working windows and os x... only runs correctly if compiled with mono....

--- trunk/WindowsBranch/LibLastRip/PlatformSettings.cs
+++ trunk/WindowsBranch/LibLastRip/PlatformSettings.cs
@@ -24,9 +24,30 @@ namespace LibLastRip
 				{
 					System.IO.Directory.CreateDirectory(Path);
 				}
-				return "c:\\temp\\";
+				return Path;
 			}
 		}
+		/*public const System.String PathSeparator = "/";
+		public static System.String TempFilePath
+		{
+			get
+			{
+				return PlatformSettings.TempPath + "TheLastRipper.mp3";
+			}
+		}
+		
+		public static System.String TempPath
+		{
+			get
+			{
+				System.String Path = "/tmp/";
+				if(!System.IO.Directory.Exists(Path))
+				{
+					System.IO.Directory.CreateDirectory(Path);
+				}
+				return Path;
+			}
+		}*/
 		
 	}
 }
