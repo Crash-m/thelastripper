281947e5b223860e00604278b32cf4aa2adef2b5
jopsen <jopsen@9db144e2-f328-0410-9072-b3fcc14a7c39>
2007-07-29 10:34:52 +0000

    * New repository layout. 	* PlatformSettings.cs: Eliminating the need for this file, here by deleted. 	* PlayList/PlayList.cs: Download XML to memory instead of using temp file. 	* LastManager/LastManager.Stream.cs: Searches for SYNC-byte order to detect songs precisely.

---
+++ libLastRip/PlatformSettings.cs
@@ -0,0 +1,31 @@
+
+using System;
+
+namespace LibLastRip
+{
+	
+	public class PlatformSettings
+	{
+		/*public const System.String PathSeparator = "\\";
+		public static System.String TempFilePath
+		{
+			get
+			{
+				return PlatformSettings.TempPath + "TheLastRipper.mp3";
+			}
+		}
+		
+		public static System.String TempPath
+		{
+			get
+			{
+				System.String Path = "c:\\temp\\";
+				if(!System.IO.Directory.Exists(Path))
+				{
+					System.IO.Directory.CreateDirectory(Path);
+				}
+				return "c:\\temp\\";
+			}
+		}*/
+	}
+}
