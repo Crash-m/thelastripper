b3e77f47cc714401f6fbfb97de59a0d5fd0893c0
jopsen <jopsen@9db144e2-f328-0410-9072-b3fcc14a7c39>
2007-07-29 10:34:52 +0000

    * New repository layout. 	* PlatformSettings.cs: Eliminating the need for this file, here by deleted. 	* PlayList/PlayList.cs: Download XML to memory instead of using temp file. 	* LastManager/LastManager.Stream.cs: Searches for SYNC-byte order to detect songs precisely.

---
+++ branches/Windows.Forms/WinFormsClient/About.Designer.cs
@@ -0,0 +1,84 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: jopsen
+ * Date: 11-02-2007
+ * Time: 14:54
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace WinFormsClient
+{
+	partial class About
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
+			this.OkButton = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// OkButton
+			// 
+			this.OkButton.Location = new System.Drawing.Point(187, 268);
+			this.OkButton.Name = "OkButton";
+			this.OkButton.Size = new System.Drawing.Size(75, 23);
+			this.OkButton.TabIndex = 0;
+			this.OkButton.Text = "Close";
+			this.OkButton.UseVisualStyleBackColor = true;
+			this.OkButton.Click += new System.EventHandler(this.OkButtonClick);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(250, 250);
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// About
+			// 
+			this.AcceptButton = this.OkButton;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(275, 301);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.OkButton);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "About";
+			this.ShowIcon = false;
+			this.Text = "About TheLastRipper";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Button OkButton;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
---
+++ branches/Windows.Forms/WinFormsClient/Settings.cs
@@ -0,0 +1,150 @@
+
+using System;
+
+namespace WinFormsClient
+{
+	[SerializableAttribute]
+	public class Settings : LibLastRip.PlayListGenerator
+	{
+		protected System.String _Password = "";
+		public LibLastRip.LastManager Manager;
+		public System.Boolean SavePassword = false;
+		
+		public Settings()
+		{
+			this._MusicPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyMusic);
+			if(!System.IO.Directory.Exists(this._MusicPath))
+			{
+				System.IO.Directory.CreateDirectory(this._MusicPath);
+			}
+			this.Manager = new LibLastRip.LastManager(this._MusicPath);
+			
+			this.LaunchPreferences();
+		}
+		
+		public virtual void LaunchPreferences()
+		{
+			this.LaunchPreferences(true);
+		}
+		
+		public virtual void LaunchPreferences(System.Boolean SaveSettings)
+		{
+			//TODO: do settings
+			Preferences Pref = new Preferences(this.Manager, this);
+			Pref.ShowDialog();
+			
+			this._MusicPath = Pref.MusicPathTextBox.Text;
+			this.Manager.MusicPath = Pref.MusicPathTextBox.Text;
+			
+			this.TopTracks = Pref.TopTracksCheckBox.Checked;
+			this.RecentLovedTracks = Pref.RecentlyLovedCheckBox.Checked;
+			this.WeeklyTrackChart = Pref.WeeklyTrackChartCheckBox.Checked;
+			this.TopTracksMixed = Pref.TopTracksMixedCheckBox.Checked;
+			this.RecentLovedTracksMixed = Pref.RecentlyLovedMixedCheckBox.Checked;
+			this.WeeklyTrackChartMixed = Pref.WeeklyTrackChartMixedCheckBox.Checked;
+			this.Mixed = Pref.AllMixedCheckBox.Checked;
+			this.m3u = Pref.M3UCheckBox.Checked;
+			this.pls = Pref.PLSCheckBox.Checked;
+			this.smil = Pref.SMILCheckBox.Checked;
+			
+			this.SavePassword = Pref.SavePasswordCheckBox.Checked;
+			
+			this._UserName = Pref.UserNameTextBox.Text;
+			if(!Pref.HasPassword)
+			{
+				this._Password = LibLastRip.LastManager.CalculateHash(Pref.PasswordTextBox.Text);
+			}
+			
+			if(SaveSettings)
+			{
+				this.SaveSettings();
+			}
+		}
+		
+		public static Settings Restore()
+		{
+			System.String AppData = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData);
+			if(System.IO.File.Exists(AppData + "\\TheLastRipper.xml"))
+			{
+					Settings Obj;
+					System.Runtime.Serialization.Formatters.Binary.BinaryFormatter Formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+					System.IO.FileStream Stream = System.IO.File.OpenRead(AppData + "\\TheLastRipper.xml");
+				
+					try
+					{
+						Obj = (Settings)Formatter.Deserialize(Stream);
+						Stream.Close();
+						Obj.SaveSettings();
+					}
+					catch
+					{
+						Stream.Close();
+						Obj = new Settings();
+					}
+				return Obj;
+			}else{
+				return new Settings();
+			}
+		}
+		public virtual void SaveSettings()
+		{
+			System.String AppData = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData);		
+			System.Runtime.Serialization.Formatters.Binary.BinaryFormatter Formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+			
+			System.IO.FileStream Stream = System.IO.File.Create(AppData + "\\TheLastRipper.xml");
+			
+			Formatter.Serialize(Stream,this);
+			Stream.Flush();
+			Stream.Close();
+		}
+		
+		//Don't mess with these variables during runtime
+		public System.String UserName
+		{
+			get
+			{
+				return this._UserName;
+			}
+		}
+		public System.String Password
+		{
+			get
+			{
+				return this._Password;
+			}
+		}
+		public System.String MusicPath
+		{
+			get
+			{
+				return this._MusicPath;
+			}
+		}
+		
+		protected Settings(System.Runtime.Serialization.SerializationInfo Info, System.Runtime.Serialization.StreamingContext context):base(Info, context)
+		{
+			if(Info.GetBoolean("HasPassword"))
+			{
+				this._Password = Info.GetString("Password");
+				this.SavePassword = true;
+			}
+			this.Manager = new LibLastRip.LastManager(this._MusicPath);
+			
+			this.LaunchPreferences(false);
+		}
+		
+		public override void GetObjectData(System.Runtime.Serialization.SerializationInfo Info, System.Runtime.Serialization.StreamingContext context)
+		{
+			base.GetObjectData(Info, context);
+			if(this.Manager.ConnectionStatus == LibLastRip.ConnectionStatus.Created)
+			{
+				this.SavePassword = false;
+			}
+			Info.AddValue("HasPassword",this.SavePassword);
+			if(this.SavePassword)
+			{
+				Info.AddValue("Password",this._Password);
+			}
+		}
+	}
+}
