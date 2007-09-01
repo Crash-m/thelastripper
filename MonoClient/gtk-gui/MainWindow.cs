c438489dcbf5d038570a9fc4165bb222e453bb74
jopsen <jopsen@9db144e2-f328-0410-9072-b3fcc14a7c39>
2007-09-01 08:57:55 +0000

    * libLastRip/LastManager/LastManager.cs: Fixing minor bug, somehow an   empty line caused TheLastRipper to fail login

--- MonoClient/gtk-gui/MainWindow.cs
+++ MonoClient/gtk-gui/MainWindow.cs
@@ -162,7 +162,7 @@ public partial class MainWindow {
         w6.Spacing = 2;
         // Container child GtkHBox.Gtk.Container+ContainerChild
         Gtk.Image w7 = new Gtk.Image();
-        w7.Name = "image2";
+        w7.Name = "image11";
         w7.Pixbuf = Stetic.IconLoader.LoadIcon(this, "stock_media-play", Gtk.IconSize.Menu, 16);
         w6.Add(w7);
         // Container child GtkHBox.Gtk.Container+ContainerChild
@@ -192,7 +192,7 @@ public partial class MainWindow {
         w15.Spacing = 2;
         // Container child GtkHBox.Gtk.Container+ContainerChild
         Gtk.Image w16 = new Gtk.Image();
-        w16.Name = "image3";
+        w16.Name = "image12";
         w16.Pixbuf = Stetic.IconLoader.LoadIcon(this, "stock_media-next", Gtk.IconSize.Menu, 16);
         w15.Add(w16);
         // Container child GtkHBox.Gtk.Container+ContainerChild
@@ -222,7 +222,7 @@ public partial class MainWindow {
         w24.Spacing = 2;
         // Container child GtkHBox.Gtk.Container+ContainerChild
         Gtk.Image w25 = new Gtk.Image();
-        w25.Name = "image4";
+        w25.Name = "image13";
         w25.Pixbuf = Stetic.IconLoader.LoadIcon(this, "stock_3d-favourites", Gtk.IconSize.Menu, 16);
         w24.Add(w25);
         // Container child GtkHBox.Gtk.Container+ContainerChild
@@ -251,7 +251,7 @@ public partial class MainWindow {
         w33.Spacing = 2;
         // Container child GtkHBox.Gtk.Container+ContainerChild
         Gtk.Image w34 = new Gtk.Image();
-        w34.Name = "image5";
+        w34.Name = "image14";
         w34.Pixbuf = Stetic.IconLoader.LoadIcon(this, "gtk-remove", Gtk.IconSize.Menu, 16);
         w33.Add(w34);
         // Container child GtkHBox.Gtk.Container+ContainerChild
