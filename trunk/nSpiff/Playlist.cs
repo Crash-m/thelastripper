27e0324bc4e9e9870d5702c419a8ff650afce47c
jopsen <jopsen@9db144e2-f328-0410-9072-b3fcc14a7c39>
2007-09-17 21:19:06 +0000

    - nSpiff implementation done, or at least all the features needed to support last.fm, some attributes and tags are still missing, but not critical. Write XSPF files feature also missing
     - Activated FxCop on nSpiff cleaned up some stuff, but mostly worring about which rules to disable. Always remember to commit solution file from now on (to share the rule settings)!
    
    - Disabled rule CS1017: Mark assemblies with assembly version

--- trunk/nSpiff/Playlist.cs
+++ trunk/nSpiff/Playlist.cs
@@ -4,34 +4,112 @@ using System.Collections.Generic;
 
 namespace nSpiff
 {
-	//TODO: Document this class and all members
+	/// <summary>
+	/// A class for reading Xml Shareable Playlist Format files.
+	/// </summary>
 	public class Playlist : System.Collections.Generic.Queue<Track>
 	{
+		//TODO: Implement support for writing XSPF files
+		
+		/// <summary>
+		/// Create an instance of Playlist without initialization, only used by derivatives
+		/// </summary>
 		protected Playlist()
 		{
 			//Used by derivative
-			//TODO: add some default values
 		}
 		
-		public Playlist(System.String Data)
+		/// <summary>
+		/// Creates an instance of playlist populated by the XmlData
+		/// </summary>
+		/// <param name="XmlData">Xml Shareable Playlist Format</param>
+		public Playlist(System.String XmlData)
 		{
-			System.Xml.XmlDocument XmlDoc = new System.Xml.XmlDocument();
-			XmlDoc.LoadXml(Data);
+			XmlDocument XmlDoc = new XmlDocument();
+			XmlDoc.LoadXml(XmlData);
 			this._Data = XmlDoc;
 			this.ParseXml(this._Data);
 		}
 		
-		public Playlist(System.Xml.XmlNode Data)
+		/// <summary>
+		/// Create an instance of playlist populated by the given XmlNode
+		/// </summary>
+		/// <param name="Data">Root XmlNode of an XSPF</param>
+		public Playlist(XmlNode Data)
 		{
 			this._Data = Data;
 			this.ParseXml(this._Data);
 		}
 		
-		protected System.String _Title;
-		protected System.String _Creator;
-		protected System.Int32 _Version;
-		protected System.Xml.XmlNode _Data;
+		//Complying with FxCop so these can't be protected
+		private System.String __Title;
+		private System.String __Creator;
+		private System.Int32 __Version;
+		private XmlNode __Data;
+		
+		/// <summary>
+		/// Holds XmlData, null if none
+		/// </summary>
+		protected XmlNode _Data
+		{
+			get
+			{
+				return this.__Data;
+			}
+			set
+			{
+				this.__Data = value;
+			}
+		}
+		
+		/// <summary>
+		/// Holds version number, null if none
+		/// </summary>
+		protected System.Int32 _Version
+		{
+			get
+			{
+				return this.__Version;
+			}
+			set
+			{
+				this.__Version = value;
+			}
+		}
+		
+		/// <summary>
+		/// Holds creator, null if none
+		/// </summary>
+		protected System.String _Creator
+		{
+			get
+			{
+				return this.__Creator;
+			}
+			set
+			{
+				this.__Creator = value;
+			}
+		}
+		
+		/// <summary>
+		/// Holds title, null if none
+		/// </summary>
+		protected System.String _Title
+		{
+			get
+			{
+				return this.__Title;
+			}
+			set
+			{
+				this.__Title = value;
+			}
+		}
 
+		/// <summary>
+		/// Gets the title of the playlist, null if no title is provided.
+		/// </summary>
 		public virtual System.String Title
 		{
 			get
@@ -40,6 +118,9 @@ namespace nSpiff
 			}
 		}
 
+		/// <summary>
+		/// Gets the creator of the playlist, null if no creator is provided.
+		/// </summary>
 		public virtual System.String Creator
 		{
 			get
@@ -48,6 +129,9 @@ namespace nSpiff
 			}
 		}
 
+		/// <summary>
+		/// Gets the version of the playlist, null if no version is provided.
+		/// </summary>
 		public virtual System.Int32 Version
 		{
 			get
@@ -56,9 +140,43 @@ namespace nSpiff
 			}
 		}
 
-		protected virtual void ParseXml(System.Xml.XmlNode Xml)
+		/// <summary>
+		/// Populates this playlist with data from an XmlNode
+		/// </summary>
+		/// <param name="xml">Root XmlNode of an XSPF</param>
+		protected void ParseXml(XmlNode xml)
 		{
-			//TODO: Loop through the xml and populate this instance with tracks
+			//Get Creator, Title and Version
+			this._Creator = xml.SelectSingleNode("playlist/creator").InnerText;
+			this._Title = xml.SelectSingleNode("playlist/title").InnerText;
+			
+			//Get the version and try to convert it
+			System.String Version = xml.SelectSingleNode("playlist").Attributes.GetNamedItem("version").InnerText;
+			if(Version != null)
+			{
+				//Catch different exceptions, since these aren't worth dying for!
+				try
+				{
+					this._Version = System.Convert.ToInt32(Version);
+				}
+				//Bad format don't kill the app
+				catch(System.FormatException)
+				{
+					//Do nothing, just a bad value
+				}
+				//Bad big duration (probably bad format or server error) don't kill the app
+				catch(System.OverflowException)
+				{
+					//Do nothing, just a big value :)
+					//Seriously nothing cry about
+				}
+			}
+			
+			//Loop through the tracks an enqueue them
+			foreach(XmlNode xmlTrack in xml.SelectNodes("playlist/trackList/track"))
+			{
+				this.Enqueue(new Track(xmlTrack));
+			}
 		}
 	}
 }
