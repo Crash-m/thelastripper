97cc3d60b511f5a211aeb5049bf15f26db28b1a2
jopsen <jopsen@9db144e2-f328-0410-9072-b3fcc14a7c39>
2007-09-21 18:08:24 +0000

    - Splitting nScrobbler in different files.  - Creating helper methods for downloading and reading a string asynchronously

---
+++ trunk/nScrobbler/nScrobbler/nScrobbler.Login.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: q
+ * Date: 21-09-2007
+ * Time: 18:47
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+
+namespace nScrobbler
+{
+	public partial class nScrobbler
+	{
+		public virtual void Login(System.String Username, System.String PasswordMd5)
+		{
+			//TODO: Autodetect platform!
+			System.String platform = "linux";
+			
+			//Get a Uri
+			System.String Uri = "http://ws.audioscrobbler.com/radio/handshake.php?version=1.3.1.1&platform=" + platform + "&username=" + Username + "&passwordmd5=" + PasswordMd5;
+			
+			this.HttpWebRequest(Uri, new nScrobbler.HttpWebCallback(this.ParseLogin));
+		}
+		
+		protected virtual void ParseLogin(System.String Data)
+		{
+			foreach(System.String Line in Data.Split(new System.Char[]{'\n'}))
+			{
+				System.String[] Opts = Line.Split(new System.Char[]{'='});
+				
+				//Skip this line if it's bad
+				if(Opts.Length != 2)
+					continue;
+				
+				switch(Opts[0].ToLower())
+				{
+					case "session":
+					//TODO: Finish this
+				}
+			}
+		}
+	}
+}
---
+++ trunk/nScrobbler/nScrobbler/nScrobbler.RequestPlaylist.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: q
+ * Date: 21-09-2007
+ * Time: 18:53
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+
+namespace nScrobbler
+{
+	public partial class nScrobbler
+	{
+		public virtual void RequestPlaylist()
+		{
+			//TODO: Initiate playlist request
+		}
+		
+		public delegate void NewTracksEventHandler(System.Object Sender, nSpiff.Playlist Tracks);
+		public virtual event NewTracksEventHandler NewTracks;
+	}
+}
