5af56d52e17fefe1010d55981e4e9dc0155fafac
jopsen <jopsen@9db144e2-f328-0410-9072-b3fcc14a7c39>
2007-10-13 11:06:32 +0000

    Implementing a login feature and doing exception handling in nScrobbler.HttpWebRequest

---
+++ nScrobbler/LoginCommandEventArgs.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: q
+ * Date: 13-10-2007
+ * Time: 12:17
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+
+namespace nScrobbler
+{
+	//TODO: Description of LoginCommandEventArgs and members
+	public class LoginCommandEventArgs : CommandEventArgs
+	{
+		/// <summary>
+		/// Is the user a subscriber?
+		/// </summary>
+		private System.Boolean _Subscriber = false;
+		
+		/// <summary>
+		/// Gets whether or not the user is a subscriber
+		/// </summary>
+		public System.Boolean Subscriber
+		{
+			get{
+				return this._Subscriber;
+			}
+		}
+		
+		public LoginCommandEventArgs(System.Boolean Success, System.Boolean Subscriber) : base(Success, CommandType.Login)
+		{
+			this._Subscriber = Subscriber;
+		}
+		
+		public LoginCommandEventArgs(System.Boolean Success, System.Boolean Subscriber, System.Exception e) : base(Success, CommandType.Login, e)
+		{
+			this._Subscriber = Subscriber;
+		}
+	}
+}
